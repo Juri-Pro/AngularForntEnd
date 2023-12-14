b3b8ba5efd16ec5cd56b6b208f17d8a12e2f1354
Kadali1229 <prasanna.kadali@sute.app>
2023-12-14 14:08:52 +0530

    old frontend

---
+++ src/app/common-features/apiRefs.ts
@@ -0,0 +1,14 @@
+export const ApiRefs = {
+    baseURL: 'https://dentys.herokuapp.com',
+    baseContext: '/dentys/',
+    createPatient:'createconsultee',
+    createAppointment: 'createAppointment',
+    generatePdf:'pdf',
+    authentication: '/authenticate',
+    getMonthAppointment: 'getAppointmentsBasedOnDates',
+    getConsultees: 'getconsultees',
+    getAppointmentsCount: 'getAppointmentsCount',
+    searchConsultee:'getconsultees',
+    getConsultantHistory: 'getAppointmentsBasedOnId',
+    editAppointment: 'editAppointment'
+}
